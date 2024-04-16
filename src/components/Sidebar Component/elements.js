6aae617f2d8c2f733a390180139ee9f89789a2d6
giftthedeveloper <giftjeremiah001@gmail.com>
2024-04-16 14:32:30 +0100

    feat: incomplete sidebar component

--- src/components/Sidebar Component/elements.js
+++ src/components/Sidebar Component/elements.js
@@ -4,7 +4,7 @@ import logo from './dashboard_logo.png';
 
 export const MainComponent = styled.div`
   width: 15%;
-  background-color: #333;
+  background-color: white;
   height: 100%;
   display: flex;
   flex-direction: column; 
@@ -13,10 +13,64 @@ export const MainComponent = styled.div`
   
 `;
 
+export const LeftComponent = styled.div`
+    height: 60%;
+    width: 100%;
+    background-color: white;
+    text-align: left;
+    align-items: center;
+`;
+
+
+export const HeaderText = styled.h3`
+  font-size: 14px;
+  text-align: left;
+  margin-left: 30px;
+  margin-bottom: 0px;
+  color: #37393d;
+  margin-top: 0px;
+`;
+
+export const MenuItem = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+  padding-left: 20px;
+  cursor: pointer;
+  margin-left: 25px;
+  color: #37393d;
+  margin-top: 20px;
+  font-size: 15px;
+`;
+export const IconWrapper = styled.div`
+  margin-right: 10px;
+`;
+
+export const RightComponent = styled.div`
+    height: 30%;
+    width: 100%;
+    background-color: white;
+    text-align: left;
+    align-items: center;
+`;
+
+export const BottomComponent = styled.div`
+    height: 10%;
+    width: 100%;
+    background-color: white;
+    text-align: left;
+    align-items: center;
+`;
+
 export const LogoComponent = styled.div`
-    width: 50px;
+    width: 100px;
+    height: 100px;
     background-image: url(${logo});
     background-size: cover;
+    padding-left: 40%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
 
 `;
--- src/components/Sidebar Component/index.js
+++ src/components/Sidebar Component/index.js
@@ -1,10 +1,66 @@
 import React from 'react'
-import { LogoComponent, MainComponent } from './elements'
+import { BottomComponent,HeaderText,MenuItem, IconWrapper, LeftComponent, LogoComponent, MainComponent, RightComponent } from './elements'
+import { PersonOutlineOutlined, ShoppingCart, ShoppingCartOutlined, Speed, StackedLineChart, TaskOutlined, TrackChanges } from '@mui/icons-material';
+
 
 function SidebarComponent() {
   return (
     <MainComponent>
         <LogoComponent />
+        <HeaderText>MENU</HeaderText>
+        <LeftComponent>
+          <MenuItem>
+              <IconWrapper><Speed /></IconWrapper>
+              Dashboard
+          </MenuItem>
+
+          <MenuItem>
+              <IconWrapper><ShoppingCartOutlined /></IconWrapper>
+              Project
+          </MenuItem>
+
+          <MenuItem>
+              <IconWrapper><TrackChanges /></IconWrapper>
+              Tracking
+          </MenuItem>
+
+          <MenuItem>
+              <IconWrapper><PersonOutlineOutlined /></IconWrapper>
+              Customers
+          </MenuItem>
+          
+          <MenuItem>
+              <IconWrapper><StackedLineChart /></IconWrapper>
+              Analytics
+          </MenuItem>
+
+          <MenuItem>
+              <IconWrapper><TaskOutlined /></IconWrapper>
+              Task
+          </MenuItem>
+
+        </LeftComponent>
+        <HeaderText>PROFILE</HeaderText>
+        <RightComponent>
+
+        <MenuItem>
+              <IconWrapper><TaskOutlined /></IconWrapper>
+              Task
+          </MenuItem>
+
+          <MenuItem>
+              <IconWrapper><TaskOutlined /></IconWrapper>
+              Task
+          </MenuItem>
+          <MenuItem>
+              <IconWrapper><TaskOutlined /></IconWrapper>
+              Task
+          </MenuItem>
+
+        </RightComponent>
+        <BottomComponent>
+
+        </BottomComponent>
     </MainComponent>
         
   )
